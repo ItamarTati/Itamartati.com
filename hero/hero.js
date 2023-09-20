18652fe8219e706df7fdd0f50fba64c1e808f313
ItamarTati <itamar.tati1994@gmail.com>
2023-09-20 20:29:16 +0100

    Put it inside a web component

---
+++ hero/hero.js
@@ -0,0 +1,34 @@
+const template = document.createElement('template');
+template.innerHTML =`
+            <head>
+                <link rel="stylesheet" href="../hero/hero.css">
+                <title>Itamar Header</title>
+            </head>
+        <body>
+            <section id="hero">
+                <div id="hero-introduction">
+                    <div id="hero-cover">
+                        <div class="ball"></div>
+                        <div class="line"></div>
+                    </div>
+                    <div class="bio">
+                        <h1>
+                            Hello, I'm <span>Itamar</span></h1>
+                        <p>a customer-oriented software engineer
+                            <br>with a focus on developing performant software</p>
+                    </div>
+                </div>
+            </section>
+        </body>`
+
+class Hero extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('itamar-hero', Hero)
